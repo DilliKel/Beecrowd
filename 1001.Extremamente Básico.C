fe3c1e3d8af2c4f9f8ea73c73d940a5199085b9f
Kelvin Araújo Ferreira <kelvin.ufrr@gmail.com>
2025-03-20 10:50:33 -0400

    Create 1001.Extremamente Básico.C

---
+++ 1001.Extremamente Básico.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, X;
+    
+    // Leitura dos valores inteiros A e B
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    // Soma de A e B
+    X = A + B;
+    
+    // Imprimir o resultado conforme o formato solicitado
+    printf("X = %d\n", X);
+    
+    return 0;
+}
